da424925cc6964374fe6f3d650b88f6fa73ec434
Peter Brumblay <pbrumblay@users.noreply.github.com>
2026-02-24 06:57:46 -0700

    fix: caching during build

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,19 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
+import { join } from 'node:path';
 import type { NextConfig } from 'next';
-
 console.log('[next.config] cwd:', process.cwd());
+console.log('[next.config] dirname:', import.meta.dirname);
 
 const nextConfig: NextConfig = {
-	cacheComponents: true,
-	serverExternalPackages: ['harperdb'],
-	cacheHandler: './lib/cache/isr-cache-handler.mjs',
-	cacheHandlers: {
-		default: './lib/cache/use-cache-handler.mjs',
-	},
-	cacheMaxMemorySize: 0,
-	logging: {
-		fetches: { fullUrl: true },
-	},
+    cacheComponents: true,
+    serverExternalPackages: ['harperdb'],
+    cacheHandler: join(import.meta.dirname, 'lib/cache/isr-cache-handler.mjs'),
+    cacheHandlers: {
+        default: join(import.meta.dirname, 'lib/cache/use-cache-handler.mjs'),
+    },
+    cacheMaxMemorySize: 0,
+    logging: {
+        fetches: { fullUrl: true },
+    },
 };
 
 export default nextConfig;
